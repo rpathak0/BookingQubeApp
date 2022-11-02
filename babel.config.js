baca09c4a52ee806d460323f3e0221ac3dda2fda
Deepak Panwar <deeepak.pro@gmail.com>
2022-11-02 11:21:21 +0530

    ios working

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['react-native-reanimated/plugin'],
+  // plugins: ['react-native-reanimated/plugin'], don't add it or it'll through error
 };
