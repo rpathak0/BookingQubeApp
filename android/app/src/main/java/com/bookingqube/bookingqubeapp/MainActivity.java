cfaf29e37662b7a745a45a2d6bb3b8e6d78c41fd
Deepak Panwar <deeepak.pro@gmail.com>
2022-11-04 15:45:12 +0530

    rename project

--- android/app/src/main/java/com/bookingqube/bookingqubeapp/MainActivity.java
+++ android/app/src/main/java/com/bookingqube/bookingqubeapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.event;
+package com.bookingqube.bookingqubeapp;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "event";
+    return "BookingQube";
   }
 
     @Override
