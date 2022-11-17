78128194655f980c9d4bb16963d99cf2098a98b0
Deepak Panwar <deeepak.pro@gmail.com>
2022-11-17 14:54:32 +0530

    rename app

--- android/app/src/main/java/com/bookingqube/bookingqube/MainApplication.java
+++ android/app/src/main/java/com/bookingqube/bookingqube/MainApplication.java
@@ -1,4 +1,4 @@
-package com.bookingqube.bookingqubeapp;
+package com.bookingqube.bookingqube;
 
 import android.app.Application;
 import android.content.Context;
@@ -71,7 +71,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.bookingqube.bookingqubeapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.bookingqube.bookingqube.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
