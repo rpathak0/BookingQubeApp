1780439c7b50528b7917bbbb1d4554f24c8efb31
Deepak Panwar <deeepak.pro@gmail.com>
2022-11-17 13:43:11 +0530

    upadte babel

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
-/* eslint-disable prettier/prettier */
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  // plugins: ['react-native-reanimated/plugin'], don't add it or it'll through error
+module.exports = function(api) {
+  api.cache(true);
+  return {
+      presets: ['module:metro-react-native-babel-preset'],
+  };
 };
